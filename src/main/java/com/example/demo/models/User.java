557393fe80507b756104a30199fa242fd276cb81
Eric Sant'Anna <ericsantanna@directv.com>
2024-06-16 17:06:52 -0400

    Initial commit

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -2,9 +2,12 @@ package com.example.demo.models;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "users")
 public class User {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
@@ -23,5 +26,26 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            '}';
     }
 }
--- src/test/java/com/example/demo/controllers/UserControllerTest.java
+++ src/test/java/com/example/demo/controllers/UserControllerTest.java
@@ -4,20 +4,31 @@ import com.example.demo.models.User;
 import com.example.demo.services.UserService;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class UserControllerTest {
 
+    private UserService userService;
+
     @Test
     public void testSave() {
-        User user = new User();
-        user.setName("John Doe");
+        User mockUser = new User();
+        mockUser.setName("John Doe");
 
         UserService userService = mock(UserService.class);
-        when(userService.save(user)).thenReturn(user);
+        when(userService.save(mockUser)).thenReturn(mockUser);
 
         UserController userController = new UserController(userService);
-        userController.save(user);
+        var user = userController.save(mockUser);
+
+        User expectedUser = new User();
+        expectedUser.setId(1L);
+        expectedUser.setName("Mario");
+
+        assertEquals(expectedUser, user, "User should be saved successfully.");
+        assertTrue(false);
     }
 }
